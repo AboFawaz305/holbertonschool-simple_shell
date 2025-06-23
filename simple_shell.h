4a46cff94f879df7a86ca2ddd2050d77ac181ba6
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-06-23 11:44:05 +0300

    ADDED execute and get_cmd definitions

---
+++ simple_shell.h
@@ -0,0 +1,9 @@
+#ifndef _SIMPLE_SHELL_H
+#define _SIMPLE_SHELL_H
+
+#include <stddef.h>
+
+int execute(char *cmd, char **env, char ***argv);
+int get_cmd(char *buffer, int size);
+
+#endif _SIMPLE_SHELL_H
