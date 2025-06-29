f324412ceaca1f6ec72afcf3f4169ff2cb4b6ee4
Abdulrahman <10939@holbertonstudents.com>
2025-06-29 14:08:34 +0300

    added is_in_path

--- execute.c
+++ execute.c
@@ -6,48 +6,79 @@
 #include "simple_shell.h"
 
 /**
-* execute - execute a command
-* @cmd: the command to execute
-* @env: environment variables
-* @argv: command arguments
-*
-* Return: 0 on success, -1 on failure
-*/
+ * execute - execute a command
+ * @cmd: the command to execute
+ * @env: environment variables
+ * @argv: command arguments
+ *
+ * Return: 0 on success, -1 on failure
+ * /
 int execute(char *cmd, char **env, char **argv)
 {
-pid_t pid;
-int status;
-char *cmd_argv[2];
+	pid_t pid;
+	int status;
+	char *cmd_argv[2];
 
-(void)argv;
+	(void)argv;
 
-if (!cmd || strlen(cmd) == 0)
-return (0);
+	if (!cmd || strlen(cmd) == 0)
+		return (0);
 
 
-cmd_argv[0] = cmd;
-cmd_argv[1] = NULL;
+	cmd_argv[0] = cmd;
+	cmd_argv[1] = NULL;
 
-pid = fork();
-if (pid == -1)
-{
-perror("fork");
-return (-1);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+
+		if (execve(cmd, cmd_argv, env) == -1)
+		{
+			perror(cmd);
+			exit(127);
+		}
+	}
+	else
+	{
+
+		wait(&status);
+	}
+
+	return (status);
 }
-else if (pid == 0)
-{
+*/
 
-if (execve(cmd, cmd_argv, env) == -1)
+/**
+ * is_in_path - checks whether command exists in $PATH or not
+ * @cmd: command name
+ *
+ * Return: 1 if cmd exists in $PATH or 0 otherwise
+ */
+int is_in_path(char *cmd, char **env)
 {
-perror(cmd);
-exit(127);
-}
-}
-else
-{
+	char *path;
 
-wait(&status);
-}
-
-return (status);
+	if (cmd == NULL)
+		return (0);
+	if (access(cmd, X_OK) == 0)
+		return (1);
+	path = strtok(get_env("PATH", env), ":");
+	while (path != NULL)
+	{
+		char *dest = malloc (sizeof (char) *(strlen(path) + 1 + strlen(cmd) + 1));
+		dest[0] = '\0';
+		strcat(dest, path);
+		strcat(dest, "/");
+		strcat(dest, cmd);
+		if (access(dest, X_OK) == 0)
+			return (1);
+		path = strtok(NULL, ":");
+		free (dest);
+	}
+	return (0);
 }
