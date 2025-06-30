ec73d958f3bb35ea2eeb2bc1e108a4a6321aefcc
Abdulrahman9907 <10939@holbertonstudents.com>
2025-06-30 14:27:43 +0300

    ADDED is_in_path
    * execute upload
    
    * execute function upload
    
    * added get_env
    
    * ADDED is_in_path
    
    ---------
    
    Co-authored-by: AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>

--- simple_shell.h
+++ simple_shell.h
@@ -5,6 +5,8 @@
 
 int execute(char *cmd, char **env, char **argv);
 int get_cmd(char **buffer, size_t *size);
+char *get_env(char *name, char **env);
+int is_in_path(char *cmd, char **env);
 int parse_cmd(char **cmd, char ***args, char *input);
 void free_args(char **args);
 
