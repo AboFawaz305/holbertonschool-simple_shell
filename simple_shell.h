73d3de8891ec9caec499a42fdbdedfc89ec12f45
Abdulrahman <10939@holbertonstudents.com>
2025-07-01 15:25:41 +0300

    implementing execute

--- simple_shell.h
+++ simple_shell.h
@@ -7,5 +7,6 @@ int execute(char *cmd, char **env, char **argv);
 int get_cmd(char **buffer, size_t *size);
 char *get_env(char *name, char **env);
 int is_in_path(char *cmd, char **env);
+char *get_full_path(char *cmd, char **env);
 
 #endif
