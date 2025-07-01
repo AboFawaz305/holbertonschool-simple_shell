0707b30478b8d08c7b15a9b02d4d3e205b33d015
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-07-01 19:13:53 +0300

    FIXED betty style errors

--- interactive.c
+++ interactive.c
@@ -23,13 +23,11 @@ void interactive_mode(char **env)
 	{
 		printf("#cisfun$ ");
 		fflush(stdout);
-
 		cmd_size = get_cmd(&buffer, &n);
 		if (cmd_size == 0)
 			continue;
 		if (cmd_size == -1)
 			break;
-
 		result = parse_cmd(&cmd, &args, buffer);
 		if (result == -1)
 		{
@@ -38,17 +36,15 @@ void interactive_mode(char **env)
 		}
 		if (result == -2)
 			continue;
-
 		if (is_builtin(cmd))
 			run_builtin(cmd, args, env);
 		else
 			execute(cmd, env, args);
-
 		free(cmd);
 		free_args(args);
 		cmd = NULL;
 		args = NULL;
 	}
-
 	free(buffer);
 }
+
