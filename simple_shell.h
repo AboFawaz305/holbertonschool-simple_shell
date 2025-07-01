0bdb174dd8ad3dde882bf0f58479722c4e560339
Abdulrahman9907 <10939@holbertonstudents.com>
2025-07-01 17:17:16 +0300

    implementing execute
    * execute upload
    
    * execute function upload
    
    * added get_env
    
    * added is_in_path
    
    * implementing execute
    
    * Fixed betty style errors
    
    ---------
    
    Co-authored-by: AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>

--- simple_shell.h
+++ simple_shell.h
@@ -19,6 +19,7 @@ int execute(char *cmd, char **env, char **argv);
 int get_cmd(char **buffer, size_t *size);
 char *get_env(char *name, char **env);
 int is_in_path(char *cmd, char **env);
+char *get_full_path(char *cmd, char **env);
 int parse_cmd(char **cmd, char ***args, char *input);
 void free_args(char **args);
 int exit_shell(char **args, char **env);
