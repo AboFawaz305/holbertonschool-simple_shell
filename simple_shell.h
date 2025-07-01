bca74575601decb648273a223e8da37d15554c31
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-07-01 11:39:18 +0000

    ADDED run_builtin

--- simple_shell.h
+++ simple_shell.h
@@ -3,6 +3,18 @@
 
 #include <stddef.h>
 
+/**
+ * struct builtin_cmd - structure for builtin commands
+ * @name: the command name
+ * @func: function pointer to the builtin function
+ */
+struct builtin_cmd
+{
+	char *name;
+	int (*func)(char **args, char **env);
+};
+
+
 int execute(char *cmd, char **env, char **argv);
 int get_cmd(char **buffer, size_t *size);
 char *get_env(char *name, char **env);
@@ -11,5 +23,7 @@ int parse_cmd(char **cmd, char ***args, char *input);
 void free_args(char **args);
 int exit_shell(char **args, char **env);
 int print_env(char **args, char **env);
+int is_builtin(char *cmd);
+int run_builtin(char *cmd, char **args, char **env);
 
 #endif
