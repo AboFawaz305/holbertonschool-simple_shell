3dad86b13044fb77871fc16ae441c1c756eb6f28
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-07-01 19:33:53 +0300

    Revert "ADDED non interactive mode support (#23)"
    This reverts commit 5a64c8fdc8a1ede133a7f6f494b1c54ecf9786a6.

--- main.c
+++ main.c
@@ -14,36 +14,7 @@ int main(int argc, char **argv, char **env)
 {
 	(void) argc;
 	(void) argv;
-	if (is_non_interactive_mode())
-	{
-		char *buffer = NULL, *cmd = NULL, **args = NULL;
-		size_t n = 0;
-		int cmd_size, result;
-
-		cmd_size = get_cmd(&buffer, &n);
-		if (cmd_size == 0)
-			return (0);
-		if (cmd_size == -1)
-			return (0);
-		result = parse_cmd(&cmd, &args, buffer);
-		if (result == -1)
-		{
-			fprintf(stderr, "Memory allocation error\n");
-			return (0);
-		}
-		if (result == -2)
-			return (0);
-		if (is_builtin(cmd))
-			run_builtin(cmd, args, env);
-		else
-			execute(cmd, env, args);
-		free(cmd);
-		free_args(args);
-		cmd = NULL;
-		args = NULL;
-		free(buffer);
-		return (0);
-	}
+	/* TODO: ADD non_interactive mode support(piped input). */
 	interactive_mode(env);
 	return (0);
 }
