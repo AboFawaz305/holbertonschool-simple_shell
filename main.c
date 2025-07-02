9a31cf1fcccf7d75e663e4343a220f803017f6d3
Abdulrahman <10939@holbertonstudents.com>
2025-07-02 15:37:01 +0300

    main.c changes

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "simple_shell.h"
-#include <string.h>
 
 /**
  * main - a simple shell program
@@ -13,41 +12,50 @@
  */
 int main(int argc, char **argv, char **env)
 {
-	(void) argc;
-	(void) argv;
 	if (is_non_interactive_mode())
 	{
 		char *buffer = NULL, *cmd = NULL, **args = NULL;
 		size_t n = 0;
-		int cmd_size, result;
+		int cmd_size, result, exit_status = 0;
+		int line_num = 0;
 
 		while ((cmd_size = get_cmd(&buffer, &n)) != -1)
 		{
+			line_num++;
 			if (cmd_size == 0)
 				continue;
 			result = parse_cmd(&cmd, &args, buffer);
 			if (result == -1)
 			{
 				fprintf(stderr, "Memory allocation error\n");
-				continue;
+				exit_status = 1;
+				break;
 			}
 			if (result == -2 || result == -3)
 				continue;
 			if (is_builtin(cmd))
 			{
-				if (strcmp(cmd, "exit") == 0)
-					free(buffer);
-				run_builtin(cmd, args, env);
+				exit_status = run_builtin(cmd, args, env);
+				/* builtin commands like exit will terminate the program */
 			}
 			else
-				execute(cmd, env, args);
+			{
+				exit_status = execute(cmd, env, args, line_num, argv[0]);
+			}
 			free(cmd);
 			free_args(args);
 			cmd = NULL;
 			args = NULL;
+			
+			/* In non-interactive mode, exit with command status if it fails */
+			if (exit_status != 0)
+			{
+				free(buffer);
+				exit(exit_status);
+			}
 		}
 		free(buffer);
-		return (0);
+		exit(exit_status);
 	}
 	interactive_mode(env);
 	return (0);
