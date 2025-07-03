0ce7cf5eeed7e22dbec611ce43149b5aa08824f4
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-07-03 11:37:54 +0300

    ADDED handling for C-c Events (#44)
    * ADDED handling for C-c
    
    * Removed global variables

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "simple_shell.h"
+#include <signal.h>
+#include <unistd.h>
+
+/**
+ * ctrl_c_handler - a handler for control C events
+ * @sig: the received signal number
+ */
+void ctrl_c_handler(int sig)
+{
+	(void) sig;
+	write(STDIN_FILENO, "\n", 1);
+	printf("#cisfun$ ");
+	fflush(stdout);
+}
 
 /**
  * main - a simple shell program
@@ -12,13 +26,16 @@
  */
 int main(int argc, char **argv, char **env)
 {
+	int status = 0;
+	char *prompt = "#cisfun$ ";
 	(void) argc;
 	(void) argv;
+	signal(SIGINT, ctrl_c_handler);
 	if (is_non_interactive_mode())
 	{
 		char *buffer = NULL;
 		size_t n = 0;
-		int cmd_size, status = 0, line_num = 0;
+		int cmd_size, line_num = 0;
 
 		while ((cmd_size = get_cmd(&buffer, &n)) != -1)
 		{
@@ -30,6 +47,6 @@ int main(int argc, char **argv, char **env)
 		free(buffer);
 		return (status);
 	}
-	interactive_mode(env);
-	return (0);
+	interactive_mode(env, &status, prompt);
+	return (status);
 }
